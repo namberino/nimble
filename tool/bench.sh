a62329bd429105b0c2f962d6192ba68f7d957b5b
namber0 <namnb2402@gmail.com>
2024-07-15 15:29:45 +0700

    Add benchmark script

---
+++ tool/bench.sh
@@ -0,0 +1,28 @@
+#!/usr/local/bin/bash
+
+declare -A elapsed_times # associative array
+
+NBL_FILES=$(find benchmark -name '*.nbl')
+
+for program in $NBL_FILES; do
+    echo "Running benchmark: $program"
+
+    # run and capture output
+    output=$(./bin/nimble "$program")
+    
+    # extract elapsed time
+    elapsed_time=$(echo "$output" | grep 'Elapsed:' | awk '{print $2}')
+    
+    # get program name without the path
+    program_name=$(basename "$program")
+    
+    # store data in dictionary
+    elapsed_times["$program_name"]="$elapsed_time"
+done
+
+echo
+echo "Elapsed time of all benchmarks:"
+
+for program in "${!elapsed_times[@]}"; do
+    echo "$program: ${elapsed_times[$program]}"
+done
--- tool/test.sh
+++ tool/test.sh
@@ -1,13 +1,18 @@
-#!/bin/sh
-NBL_FILES=$(find test -name '*.nbl')
+#!/usr/local/bin/bash
 
-failed=0;
+failed=0; # number of failed cases
+
+NBL_FILES=$(find test -name '*.nbl');
+
 for nbl in $NBL_FILES; do
+    # get expected output
     expected=${nbl}.expected;
+
+    # run and get difference between output and expected output
     echo "Running test case $nbl...";
     if ! ./bin/nimble $nbl | diff -u --color "$expected" -; then
         echo "Test case $nbl failed!";
-        failed=$((failed + 1));
+        failed=$((failed + 1)); # count failed cases
     fi;
 done;
 
