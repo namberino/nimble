0f605235a4a25359ff1678e0c2f82ff07a5e108a
namberino <namnb2402@gmail.com>
2024-07-30 12:10:59 +0700

    Add website for language

---
+++ web/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify, send_from_directory
+import subprocess
+import os
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory('static', 'index.html')
+
+@app.route('/run', methods=['POST'])
+def run_code():
+    code = request.json.get('code')
+    with open('temp.nbl', 'w') as f:
+        f.write(code)
+    
+    try:
+        result = subprocess.run(['../bin/nimble', 'temp.nbl'], capture_output=True, text=True, timeout=5)
+        output = result.stdout
+        error = result.stderr
+    except subprocess.TimeoutExpired:
+        output = ""
+        error = "Execution timed out."
+    
+    os.remove('temp.nbl')
+    
+    return jsonify({'output': output, 'error': error})
+
+if __name__ == '__main__':
+    app.run(debug=True)
